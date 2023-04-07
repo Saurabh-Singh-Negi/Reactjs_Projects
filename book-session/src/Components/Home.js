7509406575ef659304c4b3e890ccf352b4342f40
Saurabh <saurabhnegi0007@gmail.com>
2023-04-07 13:19:45 +0530

    added styling for Home component

--- book-session/src/Components/Home.js
+++ book-session/src/Components/Home.js
@@ -12,10 +12,16 @@ const Home = () => {
     }
   return (
     <>
-       <form className='flex flex-col items-center'>
-            <input type="text" autoFocus placeholder='Enter you name...' value={name} onChange={(e) => setName(e.target.value)}/>
-            <button type='submit' onClick={handleClick} >Proceed</button>
-        </form> 
+    <div className='container flex h-screen  items-center justify-center'>
+      <div className='flex flex-col gap-10'>
+        <h1 className='text-2xl font-bold'>Enter your name and proceed to book a session</h1>
+       <form className='flex flex-col items-center gap-2'>
+            <input type="text" autoFocus className='h-[40px] w-[250px] p-2' placeholder='Enter you name...' value={name} onChange={(e) => setName(e.target.value)}/>
+            <button type='submit' className='p-1 w-20 cursor-pointer border-2 text-white bg-blue-500' onClick={handleClick} >Proceed</button>
+        </form>
+        </div> 
+
+        </div>
     </>
   )
 }
